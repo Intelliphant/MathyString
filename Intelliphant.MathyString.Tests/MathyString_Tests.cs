82f570828500a322fd5af3d2f59030accea81a82
JeromyIrvine <jer@carbonated.io>
2017-03-23 18:31:26 -0400

    Public release
    The strings are mathed.

---
+++ Intelliphant.MathyString.Tests/MathyString_Tests.cs
@@ -0,0 +1,133 @@
+﻿using NUnit.Framework;
+
+namespace Intelliphant.MathyString.Tests
+{
+    [TestFixture]
+    public class MathyString_Tests
+    {
+        [Test]
+        public void MultiplicationByInt()
+        {
+            MathyString s = "test";
+            var result = s * 3;
+
+            Assert.AreEqual("testtesttest", result.ToString());
+        }
+
+        [Test]
+        public void DivisionByString()
+        {
+            MathyString s = "wibble wobble warble";
+            var result = s / " ";
+
+            Assert.AreEqual(3, result.Length);
+            Assert.AreEqual("wibble", result[0].ToString());
+            Assert.AreEqual("wobble", result[1].ToString());
+            Assert.AreEqual("warble", result[2].ToString());
+        }
+
+        [Test]
+        public void DivisionByInt()
+        {
+            MathyString s = "strings mathed";
+            var result = s / 3;
+
+            Assert.AreEqual(5, result.Length);
+            Assert.AreEqual("ing", result[1].ToString());
+            Assert.AreEqual("ed", result[4].ToString());
+        }
+
+        [Test]
+        public void ModulusByString()
+        {
+            MathyString s = "wibble wobble warble";
+            var result = s % " ";
+
+            Assert.AreEqual(2, result.Length);
+            Assert.AreEqual(" ", result[0].ToString());
+        }
+
+        [Test]
+        public void ModulusByInt()
+        {
+            MathyString s = "mathy";
+            var result = s % 3;
+
+            Assert.AreEqual("hy", result.ToString());
+        }
+
+        [Test]
+        public void SubtractionOfString()
+        {
+            MathyString s = "abcabc";
+            var result = s - "c";
+
+            Assert.AreEqual("abab", result.ToString());
+        }
+
+        [Test]
+        public void SubtractionOfInt()
+        {
+            MathyString s = "abc";
+            var result = s - 2;
+
+            Assert.AreEqual("a", result.ToString());
+        }
+
+        [Test]
+        public void SubtractionOfIntEqualToLength()
+        {
+            MathyString s = "abc";
+            var result = s - 3;
+
+            Assert.AreEqual("", result.ToString());
+        }
+
+        [Test]
+        public void SubtractionOfIntGreaterThanLengthGivesBack()
+        {
+            MathyString s = "abc";
+            var result = s - 5;
+
+            Assert.AreEqual(new string('\b', 2), result.ToString());
+        }
+
+        [Test]
+        public void DecrementIsJustSubtracting1()
+        {
+            MathyString s = "abc";
+            s--;
+
+            Assert.AreEqual("ab", s.ToString());
+        }
+
+        [Test]
+        public void IncrementConcatenates1()
+        {
+            MathyString s = "abc";
+            s++;
+
+            Assert.AreEqual("abc1", s.ToString());
+        }
+
+        [Test]
+        public void AdditionDoesConcatenation()
+        {
+            MathyString s = "abc";
+            var result = s + "d";
+
+            Assert.AreEqual("abcd", result.ToString());
+        }
+
+        [Test]
+        public void Test()
+        {
+            MathyString s = "abc";
+            MathyString t = "def";
+
+            var result = s + t;
+
+            Assert.AreEqual("abcdef", result.ToString());
+        }
+    }
+}
---
+++ Intelliphant.MathyString/MathyString.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Intelliphant.MathyString
+{
+    public class MathyString
+    {
+        private readonly string value;
+        public MathyString(string value) => this.value = value;
+        public override string ToString() => value;
+
+        public static implicit operator string(MathyString s) => s.value;
+
+        public static implicit operator MathyString(string s) => new MathyString(s);
+
+        public static MathyString operator *(MathyString s, int multiplicand) => string.Concat(Enumerable.Repeat(s.value, multiplicand));
+
+        public static MathyString[] operator /(MathyString s, int divisor)
+        {
+            var quotient = new List<MathyString>();
+            for (int i = 0; i < s.value.Length; i += divisor)
+            {
+                quotient.Add(s.value.Substring(i, Math.Min(divisor, s.value.Length - i)));
+            }
+            return quotient.ToArray();
+        }
+
+        public static MathyString[] operator /(MathyString s, string divisor) => s.value.Split(new string[] { divisor }, StringSplitOptions.None).Select(str => new MathyString(str)).ToArray();
+
+        public static MathyString operator %(MathyString s, int divisor) => s.value.Substring(s.value.Length - s.value.Length % divisor);
+
+        public static MathyString[] operator %(MathyString s, string divisor)
+        {
+            int count = s.value.Split(new string[] { divisor }, StringSplitOptions.None).Length;
+            return (count > 0) ? Enumerable.Repeat(divisor, count - 1).Select(str => new MathyString(str)).ToArray() : new MathyString[0];
+        }
+
+        public static MathyString operator -(MathyString s, string subtrahend) => new MathyString(s.value.Replace(subtrahend, ""));
+
+        public static MathyString operator -(MathyString s, int subtrahend)
+        {
+            int l = s.value.Length - subtrahend;
+            return (l >= 0) ? s.value.Substring(0, l) : new string('\b', Math.Abs(l));
+        }
+
+        public static MathyString operator +(MathyString s, string addend) => new MathyString(s.value + addend);
+
+        public static MathyString operator --(MathyString s) => s - 1;
+
+        public static MathyString operator ++(MathyString s) => new MathyString(s.value + 1);
+    }
+}
